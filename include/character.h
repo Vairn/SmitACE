e635fb83d3e2ebda651174b98f3c036d6fe509ac
Vairn <69882482+AdamENESS@users.noreply.github.com>
2023-05-12 14:04:08 +1000

    Add character.h header file
    This commit adds a new header file called character.h which includes several ACE managers and utilities. It also includes the fade.h library and amiTypes.h if not on an Amiga system.

---
+++ include/character.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <ace/managers/viewport/simplebuffer.h>
+#include <ace/managers/game.h>
+#include <ace/managers/system.h>
+#include <ace/managers/joy.h>
+#include <ace/managers/key.h>
+#include <ace/managers/blit.h>
+#include <ace/managers/ptplayer.h>
+#include <ace/utils/palette.h>
+#include <fade.h>
+
+#ifndef AMIGA
+#include "amiTypes.h"
+
+#endif
