ad0057ee0eea96311cdaf09211fa2b991b6b2e9c
Vairn <69882482+AdamENESS@users.noreply.github.com>
2023-05-12 16:33:05 +1000

    Add character-related structs and functions
    This commit adds new struct definitions for characters, character lists, and character parties. It also includes functions to create and destroy these structs as well as add, remove, and swap characters in a party.

--- include/character.h
+++ include/character.h
@@ -14,3 +14,63 @@
 #include "amiTypes.h"
 
 #endif
+
+
+typedef struct _character
+{
+    UBYTE _partyPos;
+    UBYTE _Level;
+    UWORD _Experience;
+    UWORD _HP;
+    UWORD _MaxHP;
+    UWORD _MP;
+    UWORD _MaxMP;
+    UBYTE _Strength;
+    UBYTE _Agility;
+    UBYTE _Vitality;
+    UBYTE _Intelligence;
+    UBYTE _Luck;
+    UBYTE _Attack;
+    UBYTE _Defense;
+    UBYTE _MagicDefense;
+    UBYTE _Hands[4]; // Max of 4 hands
+    UBYTE _Armor;
+    UBYTE _Shield;
+    UBYTE _Helmet;
+    UBYTE _Accessory[3];
+    UBYTE _Feet;
+    UBYTE _StatusEffect;
+    UBYTE _StatusEffectTime;
+    UBYTE _ElementalResistance[8];
+    UBYTE _ElementalWeakness[8];
+    UBYTE _SpellResistance[8];
+    UBYTE _SpellWeakness[8];
+    UBYTE _SpellImmunity[8];
+    UBYTE _Spells[8]; // bitmask of spells
+    UBYTE _Class;
+    UBYTE _Race;
+    char _Name[16];
+    UBYTE _Sprite;
+} tCharacter;
+
+typedef struct _characterList
+{
+    UBYTE _numCharacters;
+    tCharacter** _characters; // dynamic list of Characters
+} tCharacterList;
+
+typedef struct _characterParty
+{
+    UBYTE _numCharacters;
+    tCharacter** _characters; // dynamic list of Characters
+} tCharacterParty;
+
+tCharacter* characterCreate();
+void characterDestroy(tCharacter* character);
+tCharacterList* characterListCreate();
+void characterListDestroy(tCharacterList* characterList);
+tCharacterParty* characterPartyCreate();
+void characterPartyDestroy(tCharacterParty* characterParty);
+void characterPartyAdd(tCharacterParty* characterParty, tCharacter* character);
+void characterPartyRemove(tCharacterParty* characterParty, UBYTE partyPos);
+void characterPartySwap(tCharacterParty* characterParty, UBYTE partyPos1, UBYTE partyPos2);
--- src/misc/character.c
+++ src/misc/character.c
@@ -1,1 +1,57 @@
 #include "character.h"
+#include <ace/managers/memory.h>
+#include <ace/managers/system.h>
+
+tCharacter* characterCreate()
+{
+    tCharacter* character = (tCharacter*)memAllocFastClear(sizeof(tCharacter));
+    return character;
+}
+
+void characterDestroy(tCharacter* character)
+{
+    memFree(character, sizeof(tCharacter));
+}
+
+tCharacterList* characterListCreate()
+{
+    tCharacterList* characterList = (tCharacterList*)memAllocFastClear(sizeof(tCharacterList));
+    return characterList;
+}
+
+void characterListDestroy(tCharacterList* characterList)
+{
+    memFree(characterList, sizeof(tCharacterList));
+}
+
+tCharacterParty* characterPartyCreate()
+{
+    tCharacterParty* characterParty = (tCharacterParty*)memAllocFastClear(sizeof(tCharacterParty));
+    return characterParty;
+}
+
+void characterPartyDestroy(tCharacterParty* characterParty)
+{
+    memFree(characterParty, sizeof(tCharacterParty));
+}
+
+void characterPartyAdd(tCharacterParty* characterParty, tCharacter* character)
+{
+    characterParty->_characters[characterParty->_numCharacters] = character;
+    characterParty->_numCharacters++;
+}
+
+void characterPartyRemove(tCharacterParty* characterParty, UBYTE partyPos)
+{
+    characterDestroy(characterParty->_characters[partyPos]);
+    characterParty->_characters[partyPos] = NULL;
+    characterParty->_numCharacters--;
+}
+
+void characterPartySwap(tCharacterParty* characterParty, UBYTE partyPos1, UBYTE partyPos2)
+{
+    tCharacter* temp = characterParty->_characters[partyPos1];
+    characterParty->_characters[partyPos1] = characterParty->_characters[partyPos2];
+    characterParty->_characters[partyPos2] = temp;
+}
+
