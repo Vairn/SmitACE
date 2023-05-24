31583b832fd6c37566e229d9f4e69064a38fd5b4
Vairn <69882482+AdamENESS@users.noreply.github.com>
2023-05-24 14:53:59 +1000

    Update subproject commit and change state in smite.c
    The subproject commit has been updated. In smite.c, the state has been changed from g_sStateGame to g_sStateLogo.

--- src/smite.c
+++ src/smite.c
@@ -19,7 +19,7 @@ void genericCreate(void) {
 	keyCreate();
     mouseCreate(MOUSE_PORT_1);
 	ptplayerCreate(1);
-	stateChange(g_pStateMachineGame, &g_sStateGame);
+	stateChange(g_pStateMachineGame, &g_sStateLogo);
 	//AllocateCommandList();
 }
 
